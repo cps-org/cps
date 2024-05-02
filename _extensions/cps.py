c9269aba4ffb573e2f9322eb868e42d71626df67
Matthew Woehlke <mwoehlke.floss@gmail.com>
2024-05-02 16:24:33 -0400

    Make schema specification "machine readable"
    Add directives to our custom-domain extension to declare objects and
    attributes, and use these to specify the schema. In addition to
    providing a slightly more structured way to specify attribute
    parameters, this also makes it possible to create a data model of the
    schema which we will eventually use to generate a JSON schema that can
    be used to partially validate CPS JSON.

--- _extensions/cps.py
+++ _extensions/cps.py
@@ -1,10 +1,28 @@
 import re
+from dataclasses import dataclass
+from typing import cast
 
 from docutils import nodes
-from docutils.parsers.rst import directives, roles
+from docutils.parsers.rst import Directive, directives, roles
 from docutils.transforms import Transform
 
 from sphinx import addnodes, domains
+from sphinx.util import logging
+from sphinx.util.nodes import clean_astext
+
+logger = logging.getLogger(__name__)
+
+# =============================================================================
+def simplify_text(node):
+    paragraphs = []
+    for p in clean_astext(node).split('\n\n'):
+        paragraphs.append(p.replace('\n', ' '))
+
+    return '\n\n'.join(paragraphs)
+
+# =============================================================================
+def make_code(text, classname):
+    return nodes.literal(text, text, classes=['code', classname])
 
 # =============================================================================
 class InternalizeLinks(Transform):
@@ -56,12 +74,221 @@ class InternalizeLinks(Transform):
                 ref.replace_self(xref)
 
 # =============================================================================
+class ObjectDirective(Directive):
+    required_arguments = 1
+    optional_arguments = 0
+    has_content = True
+
+    # -------------------------------------------------------------------------
+    def run(self):
+        name = self.arguments[0]
+
+        # Create section node
+        section = nodes.section()
+        section.document = self.state.document
+        section.line = self.lineno
+        section['names'].append(f'{name}(object)')
+
+        # Create text nodes for section title
+        title = [
+            make_code(name, 'object'),
+            nodes.inline('(object)', '(object)', classes=['hidden']),
+        ]
+        section += nodes.title(name, '', *title)
+
+        # Parse object description
+        content = nodes.Element()
+        self.state.nested_parse(self.content, self.content_offset, content)
+        section += content.children
+
+        # Record section reference
+        self.state.document.note_implicit_target(section, section)
+
+        # Record object on domain
+        env = self.state.document.settings.env
+        domain = cast(CpsDomain, env.get_domain('cps'))
+        domain.note_object(name, simplify_text(content))
+
+        # Return generated nodes
+        return [section]
+
+# =============================================================================
+class AttributeDirective(Directive):
+    required_arguments = 1
+    optional_arguments = 0
+    has_content = True
+    option_spec = {
+        'type': directives.unchanged,
+        'context': lambda a: a.split(),
+        'overload': directives.flag,
+        'required': directives.flag,
+        'conditionally-required': directives.flag,
+    }
+
+    # -------------------------------------------------------------------------
+    def make_field(self, name, rawtext, body):
+        src, srcline = self.state.state_machine.get_source_and_line()
+
+        field = nodes.field()
+        field.source = src
+        field.line = srcline
+
+        field += nodes.field_name(name, name)
+        field += nodes.field_body(rawtext, *body)
+
+        return field
+
+    # -------------------------------------------------------------------------
+    def make_list(self, values, nodetype, classes):
+        content = [nodetype(values[0], values[0], classes=classes)]
+        for value in values[1:]:
+            content += [
+                nodes.Text(', '),
+                nodetype(value, value, classes=classes),
+            ]
+        return content
+
+    # -------------------------------------------------------------------------
+    def parse_type(self, typedesc):
+        if '|' in typedesc:
+            types = typedesc.split('|')
+            content = self.parse_type(types[0])
+            for t in types[1:]:
+                content += [
+                    nodes.Text(' '),
+                    nodes.inline('or', 'or', classes=['separator']),
+                    nodes.Text(' '),
+                ] + self.parse_type(t)
+
+            return content
+
+        m = re.match(r'^(list|map)[(](.*)[)]$', typedesc)
+        if m:
+            outer, inner = m.groups()
+            content = [
+                make_code(outer, 'type'),
+                nodes.Text(' of '),
+            ]
+            if outer == 'map':
+                content += [
+                    make_code('string', 'type'),
+                    nodes.Text(' to '),
+                ]
+
+            return content + self.parse_type(inner)
+
+        elif typedesc in {'string'}:
+            return [make_code(typedesc, 'type')]
+
+        else:
+            return [make_code(typedesc, 'object')]
+
+    # -------------------------------------------------------------------------
+    def run(self):
+        name = self.arguments[0]
+        typedesc = self.options['type']
+        context = self.options['context']
+        overload = 'overload' in self.options
+        required = 'required' in self.options
+        conditionally_required = 'conditionally-required' in self.options
+
+        if overload:
+            target = f'{name} ({context[0]})'
+        else:
+            target = name
+
+        if required:
+            required_text = 'Yes'
+        elif conditionally_required:
+            required_text = 'Special'
+        else:
+            required_text = 'No'
+
+        # Create section node
+        section = nodes.section()
+        section.document = self.state.document
+        section.line = self.lineno
+        section['names'].append(target)
+
+        # Create text nodes for section title
+        title = [make_code(name, 'attribute')]
+        if overload:
+            title += [
+                nodes.Text(' '),
+                nodes.inline(f'({context[0]})', f'({context[0]})',
+                             classes=['applies-to']),
+            ]
+        section += nodes.title(target, '', *title)
+
+        # Create nodes for attribute information
+        fields = nodes.field_list()
+        fields += self.make_field(
+            'Type', typedesc, self.parse_type(typedesc))
+        fields += self.make_field(
+            'Applies To', ', '.join(context),
+            self.make_list(context, nodes.literal, ['code', 'object']))
+        fields += self.make_field(
+            'Required', required_text, [nodes.Text(required_text)])
+        section += fields
+
+        # Parse attribute description
+        content = nodes.Element()
+        self.state.nested_parse(self.content, self.content_offset, content)
+        section += content.children
+
+        # Record section reference
+        self.state.document.note_implicit_target(section, section)
+
+        # Record object on domain
+        env = self.state.document.settings.env
+        domain = cast(CpsDomain, env.get_domain('cps'))
+        domain.note_attribute(name, context, typedesc, required=required,
+                              description=simplify_text(content), node=section)
+
+        # Return generated nodes
+        return [section]
+
+# =============================================================================
+@dataclass
+class Attribute:
+    typedesc: str
+    description: str
+    required: bool
+
+# =============================================================================
+class AttributeSet:
+
+    # -------------------------------------------------------------------------
+    def __init__(self, name, context, attribute, node):
+        self.name = name
+        self.instances = [attribute]
+        self.context = {c: (0, node) for c in context}
+
+    # -------------------------------------------------------------------------
+    def overload(self, context, attribute, node):
+        i = len(self.instances)
+        self.instances.append(attribute)
+        for c in context:
+            if c in self.context:
+                logger.warning('duplicate declaration of attribute '
+                               f'{self.name!r} on object {c!r}',
+                               location=node)
+                logger.warning(f'{self.name!r} was previously declared here',
+                               location=self.context[c][1])
+            else:
+                self.context[c] = (i, node)
+
+# =============================================================================
 class CpsDomain(domains.Domain):
     name = 'cps'
 
     # -------------------------------------------------------------------------
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Site-specific directives (used for JSON schema generation)
+        self.directives['object'] = ObjectDirective
+        self.directives['attribute'] = AttributeDirective
 
         # Site-specific custom roles (these just apply styling)
         self.add_role('hidden')
@@ -79,6 +306,30 @@ class CpsDomain(domains.Domain):
         self.add_code_role('glob')
         self.add_code_role('var')
         self.add_code_role('env')
+
+    # -------------------------------------------------------------------------
+    @property
+    def objects(self):
+        return self.data.setdefault('objects', {})
+
+    # -------------------------------------------------------------------------
+    @property
+    def attributes(self):
+        return self.data.setdefault('attributes', {})
+
+    # -------------------------------------------------------------------------
+    def note_object(self, name, description):
+        if name not in self.objects:
+            self.objects[name] = description
+
+    # -------------------------------------------------------------------------
+    def note_attribute(self, name, context, typedesc,
+                       required, description, node):
+        a = Attribute(typedesc, description, required)
+        if name not in self.attributes:
+            self.attributes[name] = AttributeSet(name, context, a, node)
+        else:
+            self.attributes[name].overload(context, a, node)
 
     # -------------------------------------------------------------------------
     def add_role(self, name, styles=None, parent=roles.generic_custom_role):
